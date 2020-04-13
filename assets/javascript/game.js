44847f8226813b6160f6bc1fbea546c02de5837c
code-guy21 <alexissj16@knights.ucf.edu>
2020-04-13 08:27:58 -0400

    added functionality to stores users guess and check it against previously attempted guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,20 @@
 // prettier-ignore
 
-//game object
+//game
 const game = {
-    //lives
-        //stores number of attempts user has
+    //stores number of attempts user has
     lives: 9,
-    //attempted
-        //stored previous attempts into array
+
+    //stores previous user guesses
     attempted: [],
-    //secret_word
-        //secret word that user will attempt to solve
+    
+    //secret word that user will attempt to solve
     secret_word: null,
-    //secret
-        //secret word that will be partially revealed as user correctly guesses
+
+    //secret word that will be partially revealed as user correctly guesses
     secret: null,
-    //dictionary
-        //array that stores a list of possible secret words
+    
+    //stores a list of possible secret words
     dictionary: [
         'test1',
         'test2',
@@ -24,38 +23,42 @@ const game = {
         'test5',
         'test6',
     ] ,
-    //guess
-        //function that checks if a character is present in the secret word
+    
+    //checks if a specific character is present in the secret word
     guess: function(char) {
 
     },
-    //randomWord
-        //function that randomly selects a word from dictionary, sets secret_word to this
-        //sets secret to an empty string of the same length
+
+    //randomly selects a word from dictionary
+    //assigns this value to secret_word
+    //sets secret to an empty string of the same length
     randomWord: function() {
-        
+
     }
 };
 
 // prettier-ignore
 
-// will execute when key is pressed down
+// main event listener
 document.onkeydown = function (event) {
     //store character into variable
     const keyPress = event.key;
 
+
     //check character against characters user has already attempted
+    if(!game.attempted.includes(keyPress)){
+        // check character against characters in secret word
+                        //if character is present in secret word
+                            //reveal matched characters to user
 
-        // if user has already attempted that guess
-            //notify user
+                        //else
+                            //user will lose a chance/attempt
 
-        // else
-            // check character against characters in secret word
-                //if character is present in secret word
-                    //reveal matched characters to user
-
-                //else
-                    //user will lose a chance/attempt
-
-            //add character to list of attempted characters
+        //add character to list of attempted characters
+        game.attempted.push(keyPress)
+    }else{
+        //notify user
+        console.log('attempted')
+    }
+            
 };
