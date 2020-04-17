5ded2e67507e300bb56e5947a9374549fd0aeab0
code-guy21 <alexissj16@knights.ucf.edu>
2020-04-17 07:14:52 -0400

    added code to reset game if user has correctly guessed the secret word or if they have run out of attempts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,88 @@
-// prettier-ignore
-
 //game
 const game = {
-    //stores number of attempts user has
-    lives: 9,
-    //stores previous user guesses
-    attempted: [],
-    
-    /* secret object stores secret word,
+	//stores number of attempts user has
+	lives: 9,
+	//stores previous user guesses
+	attempted: [],
+
+	/* secret object stores secret word,
     hidden version revealed to user,
     number of letters correctly solved */
-    secret: {
-        word: null,
-        hidden: [],
-        solved: 0,
-    },
-    
-    //stores a list of possible secret words
-    dictionary: [
-        'dog',
-        'cat',
-        'hamster',
-        'bird',
-        'bunny',
-        'hedgehog',
-    ] ,
-    
-    //checks if a specific character is present in the secret word
-    guess: function(char) {
+	secret: {
+		word: '',
+		hidden: [],
+		solved: 0,
+	},
 
-    },
+	//stores a list of possible secret words
+	dictionary: ['dog', 'cat', 'hamster', 'bird', 'bunny', 'hedgehog'],
 
-    randomWord: function() {
+	//checks if a specific character is present in the secret word
+	guess: function (char) {},
 
-        /* randomly selects a word from dictionary
+	reset: function () {
+		//resets values
+		game.lives = 9;
+		game.attempted = [];
+		game.secret.word = '';
+		game.secret.hidden = [];
+		game.secret.solved = 0;
+
+		/* randomly selects a word from dictionary
         assigns this value to secret.word */
-        this.secret.word = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
-        
-        //sets secret.hidden to an empty string of the same length
-        this.secret.hidden = [...new Array(this.secret.word.length)].map(()=> '_');
-    }
+		this.secret.word = this.dictionary[
+			Math.floor(Math.random() * this.dictionary.length)
+		];
+
+		//sets secret.hidden to an empty string of the same length
+		this.secret.hidden = [...new Array(this.secret.word.length)].map(() => '_');
+	},
 };
 
-game.randomWord();
-
-// prettier-ignore
+game.reset();
 
 // main event listener
 document.onkeydown = function (event) {
-    //store character into variable
-    const keyPress = event.key.toLowerCase();
+	//store key press into variable
+	const keyPress = event.key.toLowerCase();
 
-    //check character against characters user has already attempted
-    if(!game.attempted.includes(keyPress)){
-       
-        let matches = 0;
-        // check character against characters in secret word
-        // if there is match, reveal character in hidden word
-        for(let i=0;i<game.secret.word.length;i++){
-            if(game.secret.word[i] === keyPress){
-                game.secret.hidden[i] = game.secret.word[i];
-                matches++;
-            }
-        }
-        //else
-        //user will lose a chance/attempt
-        if(matches === 0){
-            game.lives--;
-            console.log('wrong ' + keyPress)
-        }
-        
-        //add character to list of attempted characters
-        game.attempted.push(keyPress)
-        console.log(game.secret.hidden.join(' '));
-    }else{
-        //notify user
-        console.log('attempted')
-    }
-            
+	//check character against characters user has already attempted
+	if (!game.attempted.includes(keyPress)) {
+		// set initial character matches to false
+		let matches = false;
+
+		// check character against characters in secret word
+		// if there is match, update character in hidden word, set matches to true, update total solved characters
+		for (let i = 0; i < game.secret.word.length; i++) {
+			if (game.secret.word[i] === keyPress) {
+				game.secret.hidden[i] = game.secret.word[i];
+				game.secret.solved++;
+				matches = true;
+			}
+		}
+
+		//add character to list of attempted characters
+		game.attempted.push(keyPress);
+		//reveal current hidden word
+		console.log(game.secret.hidden.join(' '));
+
+		//check if user has guessed every character correctly
+		if (matches && game.secret.solved === game.secret.word.length) {
+			//reset game if secret word has been solved
+			console.log('You Win!');
+			game.reset();
+		} else if (!matches) {
+			game.lives--;
+			console.log('wrong guess lives remaining: ' + game.lives);
+
+			// if user is out of lives, reset game
+			if (game.lives === 0) {
+				console.log('game over!');
+				game.reset();
+			}
+		}
+	} else {
+		//notify user if character has been previously attempted
+		console.log(keyPress + ' has been attempted');
+	}
 };
